997f07b7e0e245a7b7e80eaedae59b96566b4c61
Paschal Obba <paschalobba@gmail.com>
2023-11-20 01:15:44 +0000

    add styling

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Icon from "@mdi/react";
+import { mdiStar, mdiStarOutline, mdiMenuUp, mdiMenuDown } from "@mdi/js";
 import "../index.css";
+import data from "../data";
 
 const CoinList = () => {
   const [coins, setCoins] = useState([]);
@@ -9,10 +12,10 @@ const CoinList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=stablecoins&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-        );
-        setCoins(response.data);
+        // const response = await axios.get(
+        //   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=stablecoins&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        // );
+        setCoins(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -34,7 +37,7 @@ const CoinList = () => {
       <table className="coin-table">
         <thead>
           <tr>
-            <th>Serial No.</th>
+            <th>#</th>
             <th>Coin</th>
             <th>Price</th>
             <th>1h</th>
@@ -47,31 +50,96 @@ const CoinList = () => {
         <tbody>
           {coins.map((coin, index) => (
             <tr key={coin.id}>
-              <td>{index + 1}</td>
               <td>
                 {/* Check if the coin is in favorites */}
                 <span
-                  style={{ cursor: "pointer" }}
+                  style={{ cursor: "pointer", float: "left" }}
                   onClick={() => toggleFavorite(coin.id)}
                 >
                   {favorites.includes(coin.id) ? (
-                    <i className="fas fa-star"></i>
+                    <Icon path={mdiStar} size={1} />
                   ) : (
-                    <i className="far fa-star"></i>
+                    <Icon path={mdiStarOutline} size={1} />
                   )}
                 </span>
+                <span style={{ float: "right" }}>{index + 1}</span>
+              </td>
+              <td class="flatten">
+                <img src={coin.image} alt="Coin" />
+                <p>
+                  <strong>{coin.name}</strong>
+                </p>
+                <p>{(coin.symbol || "").toUpperCase()}</p>
               </td>
               <td>
-                <img src={coin.image} alt="Coin" />
-                <p>{coin.name}</p>
-                <p>{coin.symbol}</p>
+                <strong>
+                  $
+                  {(coin.current_price || 0).toLocaleString("en-US", {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </strong>
               </td>
-              <td>{coin.current_price}</td>
-              <td>{coin.price_change_percentage_1h}</td>
-              <td>{coin.price_change_percentage_24h}</td>
-              <td>{coin.price_change_percentage_7d}</td>
-              <td>{coin.total_volume}</td>
-              <td>{coin.market_cap}</td>
+              <td>
+              <span
+                  style={{ cursor: "pointer", float: "left" }}
+                >
+                  {(coin.market_cap_change_percentage_24h || 0) > 0  ? (
+                    <Icon path={mdiMenuUp} color="green" size={1} />
+                  ) : (
+                    <Icon path={mdiMenuDown} color="red" size={1} />
+                  )}
+                </span>
+                <span style={{ float: "right", color: (coin.market_cap_change_percentage_24h || 0) > 0  ? "green": "red" }}>
+                {((coin.market_cap_change_percentage_24h || 0) * -1).toFixed(1)}% 
+                </span>
+              </td>
+              {/* there is no data for 1h and 7d */}
+              <td><span
+                  style={{ cursor: "pointer", float: "left" }}
+                >
+                  {(coin.price_change_percentage_24h || 0) > 0  ? (
+                    <Icon path={mdiMenuUp} color="green" size={1} />
+                  ) : (
+                    <Icon path={mdiMenuDown} color="red" size={1} />
+                  )}
+                </span>
+                <span style={{ float: "right", color: (coin.price_change_percentage_24h || 0) > 0  ? "green": "red" }}>
+                {((coin.price_change_percentage_24h || 0) * -1).toFixed(1)}% 
+                </span></td>
+              <td>
+              <span
+                  style={{ cursor: "pointer", float: "left" }}
+                >
+                  {(coin.market_cap_change_percentage_24h || 0) > 0  ? (
+                    <Icon path={mdiMenuUp} color="green" size={1} />
+                  ) : (
+                    <Icon path={mdiMenuDown} color="red" size={1} />
+                  )}
+                </span>
+                <span style={{ float: "right", color: (coin.market_cap_change_percentage_24h || 0) > 0  ? "green": "red" }}>
+                {((coin.market_cap_change_percentage_24h || 0) * -1).toFixed(1)}% 
+                </span>
+              </td>
+              {/* there is no data for 1h and 7d */}
+              <td>
+                <strong>
+                  $
+                  {(coin.total_volume || 0).toLocaleString("en-US", {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </strong>
+              </td>
+              <td>
+                <strong>
+                  $
+                  {(coin.market_cap || 0).toLocaleString("en-US", {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </strong>
+              </td>
             </tr>
           ))}
         </tbody>
